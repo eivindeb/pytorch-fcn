dc98927898ba42962a33e562806461b8a6077250
Eivind Bøhn <eivindeb@stud.ntnu.no>
2017-11-03 17:26:49 +0100

    add log folder to checkpoint so that it continues writing to same folder on resume

--- examples/radar/train_fcn32s.py
+++ examples/radar/train_fcn32s.py
@@ -128,10 +128,10 @@ def main():
         model.load_state_dict(checkpoint['model_state_dict'])
         start_epoch = checkpoint['epoch']
         start_iteration = checkpoint['iteration']
-    #else:
-        #vgg16 = torchfcn.models.VGG16(pretrained=False)
-        #model.copy_params_from_vgg16(vgg16)
-        #model_vgg =
+        out = checkpoint['out']
+    else:
+        vgg16 = torchfcn.models.VGG16(pretrained=False)
+        model.copy_params_from_vgg16(vgg16)
     if cuda:
         model = model.cuda()
 
