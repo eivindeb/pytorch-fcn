695d26367851fd2383229da23e96e2c1d65704fb
Eivind Bøhn <eivindeb@stud.ntnu.no>
2017-11-21 20:07:44 +0100

    change configuration

--- examples/radar/train_fcn32s.py
+++ examples/radar/train_fcn32s.py
@@ -18,11 +18,11 @@ configurations = {
     # same configuration as original work
     # https://github.com/shelhamer/fcn.berkeleyvision.org
     1: dict(
-        max_iteration=100000,
-        lr=1.0e-10*0.04,  # the standard learning rate for VOC images (500x375) multiplied by ratio of radar dataset image size (4096x1000)
+        max_iteration=200000,
+        lr=1.0e-10*0.07,  # the standard learning rate for VOC images (500x375) multiplied by ratio of radar dataset image size (1365x2000)
         momentum=0.99,
         weight_decay=0.0005,
-        interval_validate=2,
+        interval_validate=4915,
     )
 }
 
